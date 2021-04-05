67815d50a2482ff98d37b9f1056dcaff0b9c0375
“Santiago <santiagoas77@gmail.com>
2021-04-05 13:51:05 -0500

    Initial commit

---
+++ src/components/Carousel.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="car">
+        <carousel :perPage="1" :autoplay="true" :autoplayTimeout="5000" :autoplayHoverPause="true" :loop="true">
+            <slide>
+                <div class="image">
+                    <img class="images" src="../assets/1.png" alt="first">
+                </div>
+            </slide>
+            <slide>
+                <img class="images" src="../assets/2.png" alt="second">
+            </slide>
+            <slide>
+                <img class="images" src="../assets/3.jpg" alt="third">
+            </slide>
+            <slide>
+                <img class="images" src="../assets/4.jpg" alt="fourth">
+            </slide>
+        </carousel>
+    </div>
+</template>
+
+<style>
+    .car{
+        min-height: 80%;
+        min-height: 80vh;
+
+        display: flex;
+        align-items: center;
+
+    }
+    .images{
+        width: 110%;
+    }
+
+    @media only screen and (min-width: 300px) and (max-width: 450px) {
+        .ig-col{
+            display: none;
+        }
+    }
+</style>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+export default {
+    components: {
+        Carousel,
+        Slide
+    }
+}
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <b-container class="container">
+            <b-row class="row">
+                <b-col></b-col>
+                <b-col class="col" align-self="center">
+                    <img src="../assets/logo.png" alt="logo">
+                </b-col>
+                <b-col class="ig-col" align-self="end"> 
+                    <a target="blank" href="https://www.instagram.com/complementario.mx/">
+                        <img class="ig-logo" src="../assets/instagram.png" alt="ig">
+                    </a>
+                </b-col>
+            </b-row>
+        </b-container>
+
+        <b-container>
+            <b-row>
+                <b-col>
+                    <h1 align-self="center" class="coming-soon">coming soon</h1>
+                </b-col>
+            </b-row>
+        </b-container>
+
+        <b-container>
+            <b-row>
+                <b-col>
+                    <Carousel />
+                </b-col>
+            </b-row>
+        </b-container>
+        
+    </div>
+</template>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&display=swap');
+    .coming-soon{
+        font-size: 32px;
+        font-family: 'Playfair Display', serif;
+    }
+    .ig-col{
+        margin: auto;
+    }
+    .coming-soon{
+        margin: 0 auto;
+    }
+    .ig-logo{
+        width: 15%;
+        float: right;
+    }
+
+    @media only screen and (min-width: 300px) and (max-width: 450px) {
+        .ig-col{
+            display: none;
+        }
+    }
+</style>
+
+<script>
+import Carousel from './Carousel.vue'
+export default {
+    components: {
+        Carousel
+    }
+}
+</script>
